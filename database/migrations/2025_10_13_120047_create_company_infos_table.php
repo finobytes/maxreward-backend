278e0cf49b9bc627da1303d1a52d108c69586947
solaiman-finobytes <solaiman.finobytes@gmail.com>
2025-10-14 10:59:18 +0600

    feat: company info added

---
+++ database/migrations/2025_10_13_120047_create_company_infos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('logo')->nullable()->comment('Cloudinary base URL');
+            $table->string('logo_cloudinary_id')->nullable();
+            $table->double('cr_points')->default(0)->comment('Company reserve points');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_infos');
+    }
+};
