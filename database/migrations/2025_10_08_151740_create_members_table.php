522b981f786eb6af8e6cc510c979a04fc58799d1
Aminul Islam <36276984+amrony@users.noreply.github.com>
2025-12-06 15:55:20 +0600

    last_status_change column add

--- database/migrations/2025_10_08_151740_create_members_table.php
+++ database/migrations/2025_10_08_151740_create_members_table.php
@@ -33,6 +33,7 @@ return new class extends Migration
             $table->unsignedBigInteger('merchant_id')->nullable();
             $table->unsignedBigInteger('suspended_by')->nullable()->comment('Admin ID who suspended');
             $table->unsignedBigInteger('blocked_by')->nullable()->comment('Admin ID who blocked');
+            $table->timestamp('last_status_changed_at')->nullable()->after('status');
             $table->timestamps();
             $table->softDeletes();
 
