8fb850ff676bdf1da7396d8f86bc98f36f667dab
solaiman-finobytes <solaiman.finobytes@gmail.com>
2026-02-21 11:27:33 +0600

    merchant model add

--- app/Http/Controllers/Api/Member/VoucherController.php
+++ app/Http/Controllers/Api/Member/VoucherController.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Notification;
+use App\Models\Merchant;
 
 class VoucherController extends Controller
 {
